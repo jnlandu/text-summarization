300f5141def48ced1598c5d9916b81658175bd36
= <=>
2024-10-02 13:59:11 +0000

    add groq utility to connect to groq client

---
+++ utils/api.py
@@ -0,0 +1,20 @@
+
+import os
+from grok import Groq
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
+
+
+Client = Groq(api_key = GROQ_API_KEY)
+
+class ChatRequest(BaseModel):
+    content: str
+
+# class SummarizationRequest(BaseModel):
+#     content: str
