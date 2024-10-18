52ae06dce8a7f45c37f2cc4d84cf0743a6d8c046
= <=>
2024-10-18 21:01:24 +0000

    Add Content-security-policy to try to solve mixed-content

--- backend/app/app.py
+++ backend/app/app.py
@@ -7,11 +7,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# API_URL = os.getenv("API_URL") for development
-
 app = FastAPI()
-# For cross plateform: use the CORSMiddleware to allow requests from the frontend
 
+
+@app.middleware("http")
+async def add_csp_header(request, call_next):
+    response = await call_next(request)
+    # Adding Content-Security-Policy to block or upgrade mixed content
+    response.headers["Content-Security-Policy"] = "default-src 'self'; upgrade-insecure-requests; block-all-mixed-content"
+    return response
+
+
+# For cross plateform: use the CORSMiddleware to allow requests from the frontend
 origins = [ "*"
     "https://mlops-project-3repcia0n-jeremies-projects-257f201c.vercel.app", # for production
     "http://localhost:3000", # for local development
